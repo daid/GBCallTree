dd191b2dd35f02f0f1c34ab2f273050c1ab8b5ba
Daid <daid303@gmail.com>
2023-08-10 15:48:34 +0200

    Ignore syntax errors on lines and just continue, then we get at least some result

--- generator/main.py
+++ generator/main.py
@@ -36,129 +36,122 @@ class Block:
 BLOCKS = {}
 cur_block = Block("", 0, "", None)
 def processfile(f):
-    global cur_block
     condition_list = [True]
     print(f"Processing: {f}")
     tok = Tokenizer(open(f, "rt").read())
     while tok:
-        t = tok.pop()
-        if t.isA('NEWLINE'):
-            continue
-        elif t.isA('ID', 'include'):
-            filename = tok.expect('STRING').value
-            if condition_list[-1]:
-                if os.path.exists(os.path.join(BASEPATH, os.path.dirname(f), filename)):
-                    processfile(os.path.join(BASEPATH, os.path.dirname(f), filename))
-                else:
-                    processfile(os.path.join(BASEPATH, filename))
-            tok.expect('NEWLINE')
-        elif t.isA('ID', 'incbin'):
-            filename = tok.expect('STRING').value
-            tok.expect('NEWLINE')
-        elif t.isA('ID', 'IF'):
-            res = parseExpr(tok) != 0
-            condition_list.append(res and condition_list[-1])
-            tok.expect('NEWLINE')
-        elif t.isA('ID', 'ELIF'):
-            res = parseExpr(tok)
-            condition_list[-1] = (not condition_list[-1]) and condition_list[-2] and res
-            tok.expect('NEWLINE')
-        elif t.isA('ID', 'ELSE'):
-            condition_list[-1] = (not condition_list[-1]) and condition_list[-2]
-            tok.expect('NEWLINE')
-        elif t.isA('ID', 'ENDC'):
-            condition_list.pop()
-            tok.expect('NEWLINE')
-        elif t.isA('ID') and tok.peek().isA('ID', 'equs'):
-            if condition_list[-1]:
-                tok.pop()
-                name = t.value
-                DEFINES[name] = parseExpr(tok)
-                #print(name, "equs", DEFINES[name])
-                tok.expect('NEWLINE')
+        try:
+            processNext(f, condition_list, tok)
+        except SyntaxError as e:
+            print("Error:", e)
+
+
+def processNext(f, condition_list, tok):
+    global cur_block
+    t = tok.pop()
+    if t.isA('NEWLINE'):
+        return
+    elif t.isA('ID', 'include'):
+        filename = tok.expect('STRING').value
+        if condition_list[-1]:
+            if os.path.exists(os.path.join(BASEPATH, os.path.dirname(f), filename)):
+                processfile(os.path.join(BASEPATH, os.path.dirname(f), filename))
             else:
-                while not tok.pop().isA('NEWLINE'):
-                    pass
-        elif t.isA('ID') and t.value.upper() in {"DEF", "REDEF"}:
-            if condition_list[-1]:
-                t = tok.expect('ID')
-                if tok.peek().isA('OP', '=') or tok.peek().isA('ID', 'equ'):
-                    tok.pop()
-                    DEFINES[t.value] = parseExpr(tok)
-                    #print(t.value, "=", DEFINES[t.value])
-                elif tok.peek().isA('ID', 'equs'):
-                    tok.pop()
-                    DEFINES[t.value] = parseExpr(tok)
-                    #print(t.value, "equs", DEFINES[t.value])
-                elif tok.peek().isA('ID', 'RB'):
-                    tok.pop()
-                    parseExpr(tok)
-                tok.expect('NEWLINE')
-            else:
-                while not tok.pop().isA('NEWLINE'):
-                    pass
-        elif t.isA('ID') and (tok.peek().isA('ID', 'equ') or tok.peek().isA('OP', '=')):
-            if condition_list[-1]:
+                processfile(os.path.join(BASEPATH, filename))
+        tok.expect('NEWLINE')
+    elif t.isA('ID', 'incbin'):
+        filename = tok.expect('STRING').value
+        tok.expect('NEWLINE')
+    elif t.isA('ID', 'IF'):
+        res = parseExpr(tok) != 0
+        condition_list.append(res and condition_list[-1])
+        tok.expect('NEWLINE')
+    elif t.isA('ID', 'ELIF'):
+        res = parseExpr(tok)
+        condition_list[-1] = (not condition_list[-1]) and condition_list[-2] and res
+        tok.expect('NEWLINE')
+    elif t.isA('ID', 'ELSE'):
+        condition_list[-1] = (not condition_list[-1]) and condition_list[-2]
+        tok.expect('NEWLINE')
+    elif t.isA('ID', 'ENDC'):
+        condition_list.pop()
+        tok.expect('NEWLINE')
+    elif t.isA('ID') and tok.peek().isA('ID', 'equs'):
+        if condition_list[-1]:
+            tok.pop()
+            name = t.value
+            DEFINES[name] = parseExpr(tok)
+            #print(name, "equs", DEFINES[name])
+            tok.expect('NEWLINE')
+        else:
+            while not tok.pop().isA('NEWLINE'):
+                pass
+    elif t.isA('ID') and t.value.upper() in {"DEF", "REDEF"}:
+        if condition_list[-1]:
+            t = tok.expect('ID')
+            if tok.peek().isA('OP', '=') or tok.peek().isA('ID', 'equ'):
                 tok.pop()
                 DEFINES[t.value] = parseExpr(tok)
                 #print(t.value, "=", DEFINES[t.value])
-                tok.expect('NEWLINE')
-            else:
-                while not tok.pop().isA('NEWLINE'):
-                    pass
-        elif t.isA('ID') and t.value.upper() in {'CHARMAP', 'SECTION', 'PUSHC', 'POPC', 'ASSERT', 'NEWCHARMAP', 'SETCHARMAP'}:
-            while not tok.pop().isA('NEWLINE'):
-                pass
-        elif t.isA('ID', 'JP_TABLE'):
-            cur_block.addInstr("JP_TABLE", [])
-            while not tok.pop().isA('NEWLINE'):
-                pass
-        elif t.isA('ID') and t.value in MACROS:
-            arg = []
-            args = [arg]
-            while not tok.peek().isA('NEWLINE'):
-                if tok.peek().isA('OP', ','):
-                    args.append([])
-                    arg = args[-1];
-                    tok.pop();
-                else:
-                    arg.append(tok.pop())
-            tok.pop()
-            if not args[-1]:
-                args.pop()
-            tok.pushMacro(MACROS[t.value], args)
-            cur_block.addInstr("MACRO", [a for a in arg for arg in args])
-        elif t.isA('ID') and t.value.upper() == 'SHIFT':
-            if tok.peek().isA('NEWLINE'):
-                count = 1
-            else:
-                count = parseExpr(tok)
-            tok.shiftMacroArgs(count)
-        elif t.isA('ID') and t.value.upper() == 'PURGE':
-            name = tok.expect('ID').value
-            if name in DEFINES:
-                del DEFINES[name]
+            elif tok.peek().isA('ID', 'equs'):
+                tok.pop()
+                DEFINES[t.value] = parseExpr(tok)
+                #print(t.value, "equs", DEFINES[t.value])
+            elif tok.peek().isA('ID', 'RB'):
+                tok.pop()
+                parseExpr(tok)
             tok.expect('NEWLINE')
-        elif t.isA('ID') and (tok.peek().isA('LABEL') or t.value.startswith(".")):
-            if tok.peek().isA('LABEL'):
-                tok.pop()
-            if tok.peek().isA('ID', 'macro'):
-                macro = []
-                tok.pop()
-                tok.expect('NEWLINE')
-                while True:
-                    line = tok.popRawLine()
-                    if line[2].upper().strip() == "ENDM":
-                        break
-                    macro.append((t.value, line[1], line[2]));
-                MACROS[t.value] = macro
-            elif condition_list[-1] and not re.match(r"\._[0-9A-F][0-9A-F]", t.value):
-                cur_block = Block(f, t.line_nr, t.value, cur_block)
-                assert cur_block.label not in BLOCKS, cur_block.label
-                BLOCKS[cur_block.label] = cur_block
-        elif t.isA('ID', 'MACRO'):
+        else:
+            while not tok.pop().isA('NEWLINE'):
+                pass
+    elif t.isA('ID') and (tok.peek().isA('ID', 'equ') or tok.peek().isA('OP', '=')):
+        if condition_list[-1]:
+            tok.pop()
+            DEFINES[t.value] = parseExpr(tok)
+            #print(t.value, "=", DEFINES[t.value])
+            tok.expect('NEWLINE')
+        else:
+            while not tok.pop().isA('NEWLINE'):
+                pass
+    elif t.isA('ID') and t.value.upper() in {'CHARMAP', 'SECTION', 'PUSHC', 'POPC', 'ASSERT', 'NEWCHARMAP', 'SETCHARMAP'}:
+        while not tok.pop().isA('NEWLINE'):
+            pass
+    elif t.isA('ID', 'JP_TABLE'):
+        cur_block.addInstr("JP_TABLE", [])
+        while not tok.pop().isA('NEWLINE'):
+            pass
+    elif t.isA('ID') and t.value in MACROS:
+        arg = []
+        args = [arg]
+        while not tok.peek().isA('NEWLINE'):
+            if tok.peek().isA('OP', ','):
+                args.append([])
+                arg = args[-1];
+                tok.pop();
+            else:
+                arg.append(tok.pop())
+        tok.pop()
+        if not args[-1]:
+            args.pop()
+        tok.pushMacro(MACROS[t.value], args)
+        cur_block.addInstr("MACRO", [a for a in arg for arg in args])
+    elif t.isA('ID') and t.value.upper() == 'SHIFT':
+        if tok.peek().isA('NEWLINE'):
+            count = 1
+        else:
+            count = parseExpr(tok)
+        tok.shiftMacroArgs(count)
+    elif t.isA('ID') and t.value.upper() == 'PURGE':
+        name = tok.expect('ID').value
+        if name in DEFINES:
+            del DEFINES[name]
+        tok.expect('NEWLINE')
+    elif t.isA('ID') and (tok.peek().isA('LABEL') or t.value.startswith(".")):
+        if tok.peek().isA('LABEL'):
+            tok.pop()
+        if tok.peek().isA('ID', 'macro'):
             macro = []
-            t = tok.pop()
+            tok.pop()
             tok.expect('NEWLINE')
             while True:
                 line = tok.popRawLine()
@@ -166,22 +159,36 @@ def processfile(f):
                     break
                 macro.append((t.value, line[1], line[2]));
             MACROS[t.value] = macro
-        elif t.isA('ID') and t.value.upper() in {'DB', 'DW', 'DS'}: # data
-            params = []
-            while not tok.peek().isA('NEWLINE'):
-                params.append(tok.pop())
-            tok.pop()
-            cur_block.addData(params)
-        elif t.isA('ID') and t.value.upper() in {'JP', 'JR', 'CALL', 'RST', 'RET', 'RETI', 'LD', 'LDH', 'LDI', 'LDD', 'ADD', 'ADC', 'SUB', 'SBC', 'AND', 'OR', 'XOR', 'NOP', 'CP', 'STOP', 'EI', 'DI', 'RRCA', 'INC', 'DEC', 'PUSH', 'POP', 'HALT', 'CPL', 'SLA', 'RL', 'RR', 'SWAP', 'RRA', 'SRL', 'RES', 'SET', 'BIT', 'DAA', 'SCF', 'CCF', 'RLA', 'SRA', 'RLCA', 'RLC', 'RRC'}:
-            params = []
-            while not tok.peek().isA('NEWLINE'):
-                params.append(tok.pop())
-            tok.pop()
-            cur_block.addInstr(t.value, params)
-        else:
-            print(f"Unexpected: {t}")
-            while not tok.pop().isA('NEWLINE'):
-                pass
+        elif condition_list[-1] and not re.match(r"\._[0-9A-F][0-9A-F]", t.value):
+            cur_block = Block(f, t.line_nr, t.value, cur_block)
+            assert cur_block.label not in BLOCKS, cur_block.label
+            BLOCKS[cur_block.label] = cur_block
+    elif t.isA('ID', 'MACRO'):
+        macro = []
+        t = tok.pop()
+        tok.expect('NEWLINE')
+        while True:
+            line = tok.popRawLine()
+            if line[2].upper().strip() == "ENDM":
+                break
+            macro.append((t.value, line[1], line[2]));
+        MACROS[t.value] = macro
+    elif t.isA('ID') and t.value.upper() in {'DB', 'DW', 'DS'}: # data
+        params = []
+        while not tok.peek().isA('NEWLINE'):
+            params.append(tok.pop())
+        tok.pop()
+        cur_block.addData(params)
+    elif t.isA('ID') and t.value.upper() in {'JP', 'JR', 'CALL', 'RST', 'RET', 'RETI', 'LD', 'LDH', 'LDI', 'LDD', 'ADD', 'ADC', 'SUB', 'SBC', 'AND', 'OR', 'XOR', 'NOP', 'CP', 'STOP', 'EI', 'DI', 'RRCA', 'INC', 'DEC', 'PUSH', 'POP', 'HALT', 'CPL', 'SLA', 'RL', 'RR', 'SWAP', 'RRA', 'SRL', 'RES', 'SET', 'BIT', 'DAA', 'SCF', 'CCF', 'RLA', 'SRA', 'RLCA', 'RLC', 'RRC'}:
+        params = []
+        while not tok.peek().isA('NEWLINE'):
+            params.append(tok.pop())
+        tok.pop()
+        cur_block.addInstr(t.value, params)
+    else:
+        print(f"Unexpected: {t}")
+        while not tok.pop().isA('NEWLINE'):
+            pass
 
 def isBlockEndingInstr(data):
     code, params = data
